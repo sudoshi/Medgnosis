3d562295f450535d9c6d816563270c88e6c7fea0
Sanjay Udoshi <smudoshi@gmail.com>
2025-02-24 14:51:38 -0500

    Simplify Next.js config for static export

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,42 +14,6 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     config.resolve.modules.push(__dirname)
     return config
-  },
-
-  // No API rewrites in production
-  async rewrites() {
-    return [];
-  },
-
-  // Configure response headers
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block'
-          }
-        ]
-      }
-    ]
   }
 }
 
